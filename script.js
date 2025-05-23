e4c589a4b55ddc1b275a0b90f766e8c940119632
gtdev <gtdevrandom@gmail.com>
2025-05-23 09:35:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -125,10 +125,10 @@ const audios = {
     p2: new Audio('musiques/musique3.mp3'),
 	p3: new Audio('musiques/musique4.mp3'),
 	p4: new Audio('musiques/musique5.mp3'),
-	p5: new Audio('musiques/musique6.mp3')
-	p6: new Audio('musiques/musique7.mp3')
-	p7: new Audio('musiques/musique8.mp3')
-	p8: new Audio('musiques/musique9.mp3')
+	p5: new Audio('musiques/musique6.mp3'),
+	p6: new Audio('musiques/musique7.mp3'),
+	p7: new Audio('musiques/musique8.mp3'),
+	p8: new Audio('musiques/musique9.mp3'),
 	p9: new Audio('musiques/musique10.mp3')
 };
 Object.values(audios).forEach(audio => {
